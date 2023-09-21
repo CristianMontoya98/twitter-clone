1ffe546eb655564dbb1209f9bbbdf3d712bc6d60
Christian Montoya <chrimonlo@gmail.com>
2023-09-20 21:55:37 -0500

    connect to supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,12 @@
-import Image from 'next/image';
-export default function Home() {
-	return <main>Hola twitter!</main>;
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+export default async function Home() {
+	const supabase = createServerComponentClient({ cookies });
+	const { data: posts } = await supabase.from('posts').select('*');
+	return (
+		<main>
+			Hola twitter!
+			{JSON.stringify(posts, null, 2)}
+		</main>
+	);
 }
